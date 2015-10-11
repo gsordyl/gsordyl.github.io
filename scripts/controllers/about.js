49a6ce6c38259647c541d3ea70f933aecccca561
gsordyl <gsordyl@github.io>
2015-10-11 22:53:51 +0200

    added angular, bootstrap, scaffolded with yeoman

---
+++ scripts/controllers/about.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name gsordylgithubioApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the gsordylgithubioApp
+ */
+angular.module('gsordylgithubioApp')
+  .controller('AboutCtrl', function () {
+  });
---
+++ scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name gsordylgithubioApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the gsordylgithubioApp
+ */
+angular.module('gsordylgithubioApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
